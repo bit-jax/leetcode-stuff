4cbcd91745f5df06abf0cb521f2d9a843f22fd51
bit-jax <axel.omley@gmail.com>
2021-08-31 14:40:19 -0700

    day two

--- new.py
+++ new.py
@@ -1,19 +1,49 @@
 
 
+# class Solution(object):
+#     def numberOfSteps(self, num):
+#         self.num = num
+#         newnum = self.num
+#         steps = 0
+#         while newnum != 0:
+#             if newnum % 2 == 0:
+#                 newnum /= 2
+#                 steps += 1
+#             else:
+#                 newnum -= 1
+#                 steps += 1
+#         return steps
+
+# newnum = Solution()
+
+# print(newnum.numberOfSteps(55))
+
 class Solution(object):
-    def numberOfSteps(self, num):
-        self.num = num
-        newnum = self.num
-        steps = 0
-        while newnum != 0:
-            if newnum % 2 == 0:
-                newnum /= 2
-                steps += 1
-            else:
-                newnum -= 1
-                steps += 1
-        return steps
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # output = []
+        # for i,val in enumerate(nums[:-1]):
+        #     if nums[i] + nums[i + 1] == target:
+        #         print(nums[i],nums[i+1])
+        #         output.append(i)
+        #         output.append(i+1)
+        #         return output
 
-newnum = Solution()
+        output = []
+        for i,val in enumerate(nums):
+            for e,val in enumerate(nums):
+                if e != i:
+                    if nums[i] + nums[e] == target:
+                        print(i)
+                        print(e)
+                        print(nums[i] + nums[e])
+                        output.append(i)
+                        output.append(e)
+                        return output
 
-print(newnum.numberOfSteps(55))
+new = Solution()
+print(new.twoSum([3,2,4],6))
